a0e3fded631ec65379433151eaea012e05888305
puspendrasingh639697 <puspendrasingh639697@gmail.com>
2025-06-23 17:05:31 +0530

    dot

--- index.js
+++ index.js
@@ -1,68 +1,37 @@
 const express = require('express');
 const connectDB = require('./Application/DB/db.js');
-require('dotenv').config(); // load .env variables
+require('dotenv').config();
 
-const
- {register,
-   login,
-   Listbox,
-   InputBox,
-   coachTypesectionlist,
-   biotankNamesectionlist,
-   divistiosectionlist,
-   zonesectionlist,
-   submitQuestionSet
-  } = require("./Application/Routes/All_In_One_Route");
+const {
+  register,
+  login,
+  Listbox,
+  InputBox,
+  coachTypesectionlist,
+  biotankNamesectionlist,
+  divistiosectionlist,
+  zonesectionlist,
+  submitQuestionSet
+} = require("./Application/Routes/All_In_One_Route");
+
 const app = express();
 app.use(express.json());
-require("dotenv").config();
 
 connectDB();
 
-app.post("/api/register",register);
+// Routes
+app.post("/api/register", register);
 app.post("/api/login", login);
 app.get("/api/list", Listbox);
 app.post("/api/submit", InputBox);
-app.get("/api/coach",coachTypesectionlist );
-app.get("/api/biotank",biotankNamesectionlist);
-app.get("/api/divistion",divistiosectionlist);
-app.get("/api/zone",zonesectionlist);
-app.post("/api/quiz",submitQuestionSet);
+app.get("/api/coach", coachTypesectionlist);
+app.get("/api/biotank", biotankNamesectionlist);
+app.get("/api/divistion", divistiosectionlist);
+app.get("/api/zone", zonesectionlist);
+app.post("/api/quiz", submitQuestionSet);
 
-
-
-//    const { name, lastname, email, password } = req.body;
-
-//   try {
-//     const newUser = await new RegisterModel({ name, lastname, email, password }).save();
-//     res.status(201).json({ message: "User registered successfully", user: newUser });
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
-
-// app.post("/api/login",async (req, res) => {
-//    const { email, password } = req.body;
-
-// try {
-//     const loginLog =await new LoginModel({ email }).save();
-//     if(loginLog.password == password){
-//     res.status(200).json({ message: "Login successful", loginLog });
-
-//     }else{
-//     res.status(200).json({ message: "Login Failed", loginLog });
-
-//     }
-//   } catch (error) {
-//     console.error("Login Error:", error);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
-
+// Start server
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0','192.168.1.24', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
-
-
