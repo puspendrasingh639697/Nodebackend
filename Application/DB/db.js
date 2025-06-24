eaae172c8676b406b5621100bb370123dbcbb765
puspendrasingh639697 <puspendrasingh639697@gmail.com>
2025-06-24 11:26:12 +0530

    string changes

--- Application/DB/db.js
+++ Application/DB/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://srdhjvkb:srdhjvkb@cluster0.0tmr38p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://srdhjvkb:srdhjvkb@cluster0.wbzidsq.mongodb.net/BioTank", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
